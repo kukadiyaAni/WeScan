0a46e669dc24f0baee1729b6677593e6ff5cf4d8
Aniruddh Kukadiya <kukadiya.ani@gmail.com>
2022-07-21 10:04:53 +0530

    Update EditScanViewController.swift

--- WeScan/Edit/EditScanViewController.swift
+++ WeScan/Edit/EditScanViewController.swift
@@ -151,7 +151,12 @@ public final class EditScanViewController: UIViewController {
         if #available(iOS 13.0,  *) {
             return UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0 > 20
         }else{
-         return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
+            if #available(iOS 11.0, *) {
+                return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
+            } else {
+                return false
+                // Fallback on earlier versions
+            }
         }
     }
     
